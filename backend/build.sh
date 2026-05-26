e1b1b1ee94a76b3fa53ae5b99ac642cc1b909d31
drishtirajput121-a11y <drishtirajput121@gmail.com>
2026-05-26 20:54:20 +0530

    decouple database from build phase

--- backend/build.sh
+++ backend/build.sh
@@ -4,21 +4,4 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
-# Create schema if it doesn't exist
-python -c "
-import os, psycopg2
-conn = psycopg2.connect(
-    dbname=os.environ['DB_NAME'],
-    user=os.environ['DB_USER'],
-    password=os.environ['DB_PASSWORD'],
-    host=os.environ['DB_HOST'],
-    port=os.environ['DB_PORT']
-)
-conn.autocommit = True
-cur = conn.cursor()
-cur.execute('CREATE SCHEMA IF NOT EXISTS chatox')
-conn.close()
-print('Schema ready')
-"
-
-python manage.py migrate
+# (Migrations and schema creation moved to startup phase to avoid build failures)
