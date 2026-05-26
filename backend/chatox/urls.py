609059dc680c2cba2b29c4ef976b2ce35a9f6376
drishtirajput121-a11y <drishtirajput121@gmail.com>
2026-05-26 18:20:31 +0530

    added mising import

--- backend/chatox/urls.py
+++ backend/chatox/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.http import JsonResponse
+from django.core.files.storage import default_storage
+
 def check_cloudinary(request):
     import os
     return JsonResponse({
