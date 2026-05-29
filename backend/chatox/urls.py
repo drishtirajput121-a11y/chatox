04a33bc1cdcd27b739a8030f93a422719eb269dc
drishtirajput121-a11y <drishtirajput121@gmail.com>
2026-05-29 15:17:37 +0530

    fixed health check

--- backend/chatox/urls.py
+++ backend/chatox/urls.py
@@ -18,9 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
 
+def health(request):
+    return JsonResponse({'status': 'ok'})
 
 urlpatterns = [
+    path('health/', health),
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
     path('api/tweets/', include('tweets.urls')),
