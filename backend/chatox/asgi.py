531bf908547b66ba3c4d131186b4c68efcc2e6d3
drishtirajput121-a11y <drishtirajput121@gmail.com>
2026-05-29 16:22:53 +0530

    fixed websocket connection

--- backend/chatox/asgi.py
+++ backend/chatox/asgi.py
@@ -11,7 +11,6 @@ import os
 import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatox.settings')
 django.setup()
@@ -21,9 +20,7 @@ from chat.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
-    'websocket': AllowedHostsOriginValidator(
-        JWTAuthMiddleware(
-            URLRouter(websocket_urlpatterns)
-        )
+    'websocket': JWTAuthMiddleware(
+        URLRouter(websocket_urlpatterns)
     ),
 })
